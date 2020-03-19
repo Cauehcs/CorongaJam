8ba7d3239dbc54645f2d6bce262d619191d863c8
Cauehcs <caue.hcs@gmail.com>
2020-03-19 13:03:22 -0300

    Movimentação e Código das barras inicial

---
+++ Assets/Scripts/Barras/barsBerraviour.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class barsBerraviour : MonoBehaviour
+{
+    [SerializeField] Image[] bars;
+    static float valorTedio, valorSaude, valorMedo;
+
+    private void Update() {
+        
+        valorSaude = Mathf.Clamp(valorSaude, 0, 1);
+        valorTedio = Mathf.Clamp(valorTedio, 0, 1);
+        valorMedo = Mathf.Clamp(valorMedo, 0, 1);
+
+        print("tedio " + valorTedio);
+        print("saude " + valorSaude);
+        print("medo " + valorMedo);
+
+        bars[0].fillAmount = valorTedio;
+        bars[1].fillAmount = valorSaude;
+        bars[2].fillAmount = valorMedo;
+    }
+
+    public static void ResetBars(float barra) {
+        switch (barra) {
+            case 0:
+                valorTedio = 1;
+                break;
+            case 1:
+                valorSaude = 1;
+                break;
+            case 2:
+                valorMedo = 1;
+                break;
+            case 3:
+                valorMedo = 1; valorSaude = 1; valorTedio = 1;
+                break;
+        }
+    }
+
+    public static void PerderBarra(float barra, float valor) {
+        //0.0001
+        switch (barra) {
+            case 0: valorTedio -= valor;
+                break;
+            case 1: valorSaude -= valor;
+                break;
+            case 2: valorMedo -= valor;
+                break;
+        }
+    }
+
+    public static void GanharBarra(float barra, float valor) {
+        switch (barra) {
+            case 0:
+                valorTedio += valor;
+                break;
+            case 1:
+                valorSaude += valor;
+                break;
+            case 2:
+                valorMedo += valor;
+                break;
+        }
+    }
+
+}
--- Assets/Scripts/Personagem/playerBehaviour.cs
+++ Assets/Scripts/Personagem/playerBehaviour.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class playerBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] float speed;
+    public Rigidbody2D my_Rb;
+
+    private void Awake() {
+        barsBerraviour.ResetBars(3);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate() {
+    
+        Movimentacao(speed);
+       
+        barsBerraviour.PerderBarra(0, 0.0001f * 4f);
+
+        //0.0001f / 1.5f - ~5.0min 
+        //0.0001f * 1.0f - ~3.15min
+        //0.0001f * 2.0f - ~1.45min
+        //0.0001f * 2.5f - ~1.15min
+
+    }
+
+    void Movimentacao(float speed) {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        my_Rb.velocity = new Vector2(horizontal * speed, vertical * speed);
     }
 }
